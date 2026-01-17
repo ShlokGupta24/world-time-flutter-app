e41eaaffd729cf82564373cb072ad65e4a0863ab
ShlokGupta24 <shlokgupta2402@gmail.com>
2026-01-17 16:54:32 +0530

    Replaced unstable worldtimeapi with reliable timezonedbapi

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response response = await get(
+      Uri.parse(
+        'https://api.timezonedb.com/v2.1/get-time-zone?key=Z03QNRUFEYOQ&format=json&by=zone&zone=Asia/Kolkata',
+      ),
+    );
+    Map data = jsonDecode(response.body);
+    print(data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Text("Loading Screen"));
